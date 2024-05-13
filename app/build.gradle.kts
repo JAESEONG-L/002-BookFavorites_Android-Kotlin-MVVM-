d1f3f77db1dabaf5ad682859bf7a41acf7def768
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-05-14 01:00:06 +0900

    chore: modify dependencies
    Changed library from androidx.activity:activity to androidx.activity:activity-ktx.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
