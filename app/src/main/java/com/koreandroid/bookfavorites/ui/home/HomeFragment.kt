596249c9c72f5a2c337cde3e6ab41be071586624
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-05-14 00:49:23 +0900

    add: add Home fragment with the ViewModel

---
+++ app/src/main/java/com/koreandroid/bookfavorites/ui/home/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.koreandroid.bookfavorites.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.koreandroid.bookfavorites.R
+
+class HomeFragment : Fragment() {
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
---
+++ app/src/main/java/com/koreandroid/bookfavorites/ui/home/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.koreandroid.bookfavorites.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    // TODO: Implement the ViewModel
+}
