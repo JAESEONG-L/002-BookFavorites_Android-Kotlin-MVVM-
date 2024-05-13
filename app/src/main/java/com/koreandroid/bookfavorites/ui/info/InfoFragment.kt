7573834af27632ff64532dbabbaeec454db53f4c
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-05-14 01:07:54 +0900

    add: add Info fragment with the ViewModel

---
+++ app/src/main/java/com/koreandroid/bookfavorites/ui/info/InfoFragment.kt
@@ -0,0 +1,21 @@
+package com.koreandroid.bookfavorites.ui.info
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.koreandroid.bookfavorites.R
+
+class InfoFragment : Fragment() {
+
+    private val viewModel: InfoViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+}
