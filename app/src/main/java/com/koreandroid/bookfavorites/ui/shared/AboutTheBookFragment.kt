4d389fad108b330cd91e41aa34e747a3a4a00bfe
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-05-15 21:45:55 +0900

    add: add About The Book fragment

---
+++ app/src/main/java/com/koreandroid/bookfavorites/ui/shared/AboutTheBookFragment.kt
@@ -0,0 +1,6 @@
+package com.koreandroid.bookfavorites.ui.shared
+
+import androidx.fragment.app.Fragment
+import com.koreandroid.bookfavorites.R
+
+class AboutTheBookFragment : Fragment(R.layout.fragment_about_the_book)
