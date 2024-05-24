45df83b5da9ac2ddbf86e8b380276dfae5ff599e
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-05-24 18:28:18 +0900

    feat: connect navigation graph to NavHostFragment and setup Navigation bar with the NavController

--- app/src/main/java/com/koreandroid/bookfavorites/MainActivity.kt
+++ app/src/main/java/com/koreandroid/bookfavorites/MainActivity.kt
@@ -5,17 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.koreandroid.bookfavorites.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            with(v) {
+                setPadding(systemBars.left, paddingTop, systemBars.right, systemBars.bottom)
+            }
+
             insets
         }
+
+        // Getting the NavController of the NavHostFragment connected with @id/fcv_main widget.
+        val navController =
+            (supportFragmentManager.findFragmentById(binding.fcvMain.id) as NavHostFragment).navController
+
+        setupNavigationBar(navController)
+    }
+
+    private fun setupNavigationBar(navController: NavController) {
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
